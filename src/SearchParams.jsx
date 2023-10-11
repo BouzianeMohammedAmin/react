3af0b38b76dc681e3dc732da39fa7290a05c39d4
Bouziane Mohammed Amin <itsbouzianemed@gmail.com>
2023-10-11 19:41:24 +0100

    mapping data with hook

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -4,6 +4,8 @@ React
 const SearchParams = ()=>{
 
     const [location , setlocation] = useState("");
+    const [animal  ,setAnimal] = useState("");
+    const ANIMALS = ["animal1" , "animal2" , "animal3" , "animal4"] ;
 
     return (
         <di className="search-params">
@@ -13,6 +15,15 @@ const SearchParams = ()=>{
                     <input 
                             onChange={e=>setlocation(e.target.value)}
                     id="location" value={location}  placeholder="location" ></input>
+                </label>
+
+                <label htmlFor="animal">
+                <select   value={animal}  onChange={e=>{setAnimal(e.target.value)}}>
+                {ANIMALS.map((animal)=> (
+                      <option key={animal}>{animal}</option>
+                ))} ;
+
+                </select>
                 </label>
                 <button> Submet</button>
             </form>
