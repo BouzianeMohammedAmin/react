6b09d231c612b9944dbd82e761546c5e65797fe9
Bouziane Mohammed Amin <itsbouzianemed@gmail.com>
2023-10-12 19:52:43 +0100

    disabl eslint foor hook

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -28,6 +28,7 @@ export default function useBreedList(animal) {
       setStatus("Loaded");
     }
     console.log(breedList) ;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   } , [animal]);
   return [breedList , status] ;
 }
